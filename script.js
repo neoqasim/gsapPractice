5201a4a854b11d0c53a6873b2358796658fd7455
neoqasim <neoqasim@gmail.com>
2024-08-18 16:50:24 +0500

    learnt the basics

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-gsap.to(".box1 ", {
+gsap.to("div.box1 ", {
     x: 1000, duration: 2,
+    rotate: 360 * 3,
+    yoyo: true,
+    repeat: 1,
+    delay: 1,
+    scale: 0.6
 })
-gsap.from(".box2 ", {
-    x: 1000, duration: 1,
-    rotate: 270, backgroundColor: "red" ,delay: 1
+var tl = gsap.timeline()
+
+
+tl.from("ul.logo li", {
+    y: -30, duration: 0.6, opacity: 0
+})
+
+tl.from("ul.navlinkp li", {
+    y: -30, duration: 0.6, opacity: 0,
+    stagger:0.3
 })
