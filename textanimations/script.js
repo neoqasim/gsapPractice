1f2b827a4381a58ef721995f0f000999d7ffae88
neoqasim <neoqasim@gmail.com>
2024-08-20 15:47:52 +0500

    completed the ext animations

--- textanimations/script.js
+++ textanimations/script.js
@@ -2,21 +2,36 @@ let breakText = () => {
     var h1 = document.querySelector('h1')
     var h1Text = document.querySelector('h1').textContent
     let splittedText = h1Text.split("")
+    let clutter = ""
+    let halfVal = splittedText.length / 2
 
-    var clutter = ""
-    splittedText.forEach((e) => {
-        clutter += `<span>${e}</span>`
+
+    splittedText.forEach((e, index) => {
+        if (halfVal > index) {
+            clutter += `<span class="a">${e}</span>`
+        } else {
+            clutter += `<span class="b">${e}</span>`
+
+        }
     })
+    console.log(clutter);
 
     h1.innerHTML = clutter
 }
 
 breakText()
 
-gsap.from("h1 span", {
+gsap.from("h1 span.a", {
     y: 70,
-    duration: 0.7,
+    duration: 0.5,
     opacity: 0,
     stagger: 0.1,
-    // ease: "elastic.out(1,0.2)"
+    ease: "back.out"
+})
+gsap.from("h1 span.b", {
+    y: 70,
+    duration: 0.5,
+    opacity: 0,
+    stagger: -0.1,
+    ease: "back.out"
 })
